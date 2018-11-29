9dcb0e1b2af50abc0e760f5c2e0c0205343c1c22
nvsclub <nvsclub.w@gmail.com>
2018-11-29 11:02:26 +0000

    Added objectives. Created Thread_test to run client-server in the same class.

--- README.md
+++ README.md
@@ -32,3 +32,23 @@ Eg. java HelloWorld
 ## Java Graphs visualization
 
 http://graphstream-project.org/
+
+# Anti entropy Vs Gossip : Objetivos do projeto
+Temos de criar nos e utilizá-los em topologias diferentes.
+
+Vamos definir várias topologias para depois compará-las.
+
+Os nos vão ter 2 sockets, e em uma abordagem inicial 2. 
+threads (uma servidor outra cliente).
+
+O nosso objetivo é trocar 3 bytes (RGB).
+
+Usar um programa que nos dá a topologia da rede e pintar os nos com estas RGB. Utilizar estes valores para pintar as bolas dos nos. Pintar as linhas para ver por onde passou.
+
+Ver como criar /utilizar os algoritmos.
+
+Analisar as diferentes performances (para os 2 algoritmos e para redes diferentes):
+- Percentagem de nós até onde chegou a mensagem,
+- Percentagem de nós que receberam a mensagem correta dos que receberam;
+- Percentagem de nós que receberam a mensagem correta;
+- Análise temporal;
--- Thread_test.java
+++ Thread_test.java
@@ -1,12 +1,70 @@
+import java.io.*;
+import java.net.*;
+
+// Server and client running in the same class
+
 public class Thread_test{
+  public static final int number_of_nodes = 5;
+
   public static void main(String[] argv){
-    for(int it = 0; it < 10; it++){
-      new Thread(new Runnable(){
-        public void run() {
-          System.out.println("Hello World!");
+    // Server
+    new Thread(new Runnable(){
+      public void run(){
+        try{
+          ServerSocket ss = new ServerSocket(6666);
           
+          Socket s = null;
+        
+          s = ss.accept();
+
+          System.out.println("Client connected: " + s);
+
+          DataInputStream dis = new DataInputStream(s.getInputStream());
+          DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+          while(true){
+            // Listen to the clients
+            String received = dis.readUTF();
+
+            System.out.println(received);
+
+            // Write answer
+            dos.writeUTF("Server Answers");
+          }
+        }catch (Exception e){
+          e.printStackTrace();
         }
-      }).start();
-    }
+      }
+    }).start();
+
+    // Client
+    new Thread(new Runnable(){
+      public void run() {
+        try{
+          InetAddress ip = InetAddress.getByName("localhost");
+
+          Socket s = new Socket(ip, 6666);
+
+          DataInputStream dis = new DataInputStream(s.getInputStream());
+          DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+          while(true){
+            // Request client
+            dos.writeUTF("Client Call");
+            
+            // Receive answer
+            String received = dis.readUTF();
+
+            System.out.println(received);
+
+          }
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+      }
+      
+    }).start();
+
+
   }
 }
