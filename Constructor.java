73c49a3235e34cd4a8c945879980e6d9cf645e2a
nvsclub <nvsclub.w@gmail.com>
2018-12-25 16:44:10 +0000

    Joined graphstream visualization to the constructor

--- Constructor.java
+++ Constructor.java
@@ -1,7 +1,11 @@
 import java.net.*;
 import java.util.List;
-import java.util.ArrayList;;
+import java.util.ArrayList;
 import java.util.Random;
+
+/* java graphsteam libraries */
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
 
 class Constructor{
 
@@ -15,17 +19,17 @@ class Constructor{
     int sock_1 = 15001;
     int[] conn_1 = {15002, 15003};
     int sock_2 = 15002;
-    int[] conn_2 = {15001, 15003};
+    int[] conn_2 = {15001, 15003, 15007};
     int sock_3 = 15003;
-    int[] conn_3 = {15001, 15002};
-    /*int sock_4 = 15004;
+    int[] conn_3 = {15001, 15002, 15004};
+    int sock_4 = 15004;
     int[] conn_4 = {15003, 15005};
     int sock_5 = 15005;
     int[] conn_5 = {15004, 15006};
     int sock_6 = 15006;
     int[] conn_6 = {15005, 15007};
     int sock_7 = 15007;
-    int[] conn_7 = {15005, 15006};*/
+    int[] conn_7 = {15005, 15006, 15002};
     
     /* Create the threads according to the topology */
     Thread node_1 = new Gossip_node(sock_1, conn_1);
@@ -34,41 +38,82 @@ class Constructor{
     node_2.start();
     Thread node_3 = new Gossip_node(sock_3, conn_3);
     node_3.start();
-    /*Thread node_4 = new Gossip_node(sock_4, conn_4);
+    Thread node_4 = new Gossip_node(sock_4, conn_4);
     node_4.start();
     Thread node_5 = new Gossip_node(sock_5, conn_5);
     node_5.start();
     Thread node_6 = new Gossip_node(sock_6, conn_6);
     node_6.start();
     Thread node_7 = new Gossip_node(sock_7, conn_7);
-    node_7.start();*/
+    node_7.start();
     
 
     System.out.println("Constructor: all threads initalized");
-
+    
+    
+    /* Graph stream initializations */
+    Graph graph = new SingleGraph("Tutorial 1");
+    
+    /* We can set the display in the beginning and we do not need to worry about it anymore */
+    graph.display();
+    
+    /* Adding nodes */
+    graph.addNode("15001");
+    Node node = graph.getNode("15001");
+    node.addAttribute("ui.style", "fill-color: green;");
+    graph.addNode("15002");
+    graph.addNode("15003");
+    graph.addNode("15004");
+    graph.addNode("15005");
+    graph.addNode("15006");
+    graph.addNode("15007");
+    
+    /* Adding edges */
+    Edge edge;
+    graph.addEdge("1500115002", "15001", "15002");
+    graph.addEdge("1500115003", "15001", "15003");
+    graph.addEdge("1500215003", "15002", "15003");
+    graph.addEdge("1500215007", "15002", "15007");
+    graph.addEdge("1500315004", "15003", "15004");
+    graph.addEdge("1500415005", "15004", "15005");
+    graph.addEdge("1500515006", "15005", "15006");
+    graph.addEdge("1500615007", "15006", "15007");
+    
+    System.out.println("Constructor: graph initialized");
+    
+    
+    /* Connection variables */
     DatagramSocket connection_socket;
-  
     InetAddress ip_address;
-
+    
     /* Setup socket to receive information */
     try{
       /* Get local IP address */
       ip_address = InetAddress.getByName("localhost");
       
       /* Initialize sockets
-       * Having a client socket makes it so that we cannot naturally identify
-       * the node who sent the message to us 
-       * Thats why we will use only one socket */
+      * Having a client socket makes it so that we cannot naturally identify
+      * the node who sent the message to us 
+      * Thats why we will use only one socket */
       connection_socket = new DatagramSocket(main_port);
-
-    
-
+      
+      
       /* Creating buffers */
       byte[] receive_bytes = new byte[1024];
-
+      
       /* Creating the strings to read the data */
       String receive_data = new String(); 
-          
+      
+      /* Graphstream objects to update */
+      String link_to_update = new String();
+      String node_to_update = new String();
+      
+      String node_that_sent = new String();
+      String node_that_updated = new String();
+      
+      System.out.println("Constructor: receiver port configured");
+      
+      
       /* Loop to receive the information about the network */
       while(true){
         try{
@@ -84,14 +129,46 @@ class Constructor{
           connection_socket.receive(receive_packet);
           receive_data = new String(receive_packet.getData()).trim();
           
-          System.out.println(receive_data);
+          /* Get the information received about the structure */
+          if(receive_data.length() > 5){
+            node_to_update = receive_data.substring(receive_data.length() - 5);
+
+            /* Guarantees that the order is correct in the link */
+            node_that_sent = receive_data.substring(0, receive_data.length() - 5);
+            node_that_updated = receive_data.substring(receive_data.length() - 5);
+            
+            if(Integer.parseInt(node_that_updated) > Integer.parseInt(node_that_sent)){
+              link_to_update = receive_data;
+
+            }
+            else{
+              link_to_update = node_that_updated + node_that_sent;
+            }
+            
+            /* Good practices lesson:
+             * Verify if the node/edge is found, else program will crash when it doesnt. 
+             * Here we have the certainty that it exists so we progress forward */
+            /* Coloring stuff 
+            * Node */
+            node = graph.getNode(node_to_update);
+            node.addAttribute("ui.style", "fill-color: blue;");
+            /* Coloring stuff 
+            * Link */
+            edge = graph.getEdge(link_to_update);
+            edge.addAttribute("ui.style", "fill-color: red;");
+
+            
+            
+          }
+          
+          System.out.println("Constructor: updated node " + node_that_updated + " and link " + link_to_update + node_that_sent);
           
         } catch (Exception e) { 
           e.printStackTrace(); 
         } 
-
+        
       }
-
+      
     
     } catch (Exception e) { 
       e.printStackTrace(); 
